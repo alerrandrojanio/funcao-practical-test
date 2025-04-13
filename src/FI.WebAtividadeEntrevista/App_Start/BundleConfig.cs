0c9121faba682249196870d18a89fcaceb2251ab
Alerrandro Janio <alerrandro.janio07@gmail.com>
2025-04-13 19:12:27 -0300

    feat: adição de campo cpf, adição de scripts para as telas e modal de inclusao de beneficiario

--- src/FI.WebAtividadeEntrevista/App_Start/BundleConfig.cs
+++ src/FI.WebAtividadeEntrevista/App_Start/BundleConfig.cs
@@ -44,6 +44,12 @@ namespace FI.WebAtividadeEntrevista
 
             bundles.Add(new StyleBundle("~/Content/jtable").Include(
                       "~/Scripts/jtable/themes/metro/darkgray/jtable.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/beneficiarios").Include(
+                      "~/Scripts/Beneficiarios/FI.Beneficiarios.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquerymask").Include(
+                      "~/Scripts/jquery.mask.min.js"));
         }
     }
 }
